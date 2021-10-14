67d3597428282c578b045e301d474ab2f193531b
Yahya Oukharta <youkhart@s2r2p1.1337.ma>
2021-10-14 16:52:42 +0100

    update

--- philo/main.c
+++ philo/main.c
@@ -7,14 +7,14 @@
 
 void print_status(t_philo *philo, int has_taken_fork)
 {
-    pthread_mutex_lock(philo->print_mutex);
 
+    pthread_mutex_lock(philo->print_mutex);
     if (philo->data->death_occured)
         return ;
     if(has_taken_fork)
         printf("%.6lld %d has taken a fork\n", get_time() - philo->data->init_time, philo->index + 1);
     else if(philo->is_dead)
-        printf("%.6lld %d died, last m =%.6lld\n", get_time() - philo->data->init_time, philo->index + 1, get_time() - philo->data->init_time - philo->last_meal_time);
+        printf("%.6lld %d died\n", get_time() - philo->data->init_time, philo->index + 1);
     else
     {
         char *state = (philo->state == 1 ? "eating" : (philo->state == 2 ? "sleeping":"thinking"));
@@ -25,6 +25,8 @@ void print_status(t_philo *philo, int has_taken_fork)
 
 void _sleep(void *philosopher)
 {
+    if (((t_philo *)philosopher)->data->death_occured)
+        return ;
     ((t_philo *)philosopher)->state = 2;
     print_status(((t_philo *)philosopher), 0);
     my_sleep(((t_philo *)philosopher)->data->time_to_sleep);
@@ -33,39 +35,49 @@ void _sleep(void *philosopher)
 int everyone_ate(t_data *data)
 {
     int i = 0;
-    if (data->done)
-        return (1);
+    if (data->death_occured)
+            return (0);
     if (!data->number_of_times_each_philosopher_must_eat)
         return (0);
-    while(i < data->number_of_philosophers)
+    while(i < data->number_of_philosophers )
     {
+        if (data->death_occured)
+            return (0);
         if (data->philosophers[i]->n_meals < data->number_of_times_each_philosopher_must_eat)
             return (0);
         i++;
     }
-    data->done = 1;
+
+    data->death_occured = 1;
     return (1);
 }
 
 void _eat(void *philosopher)
 {
+    if (((t_philo *)philosopher)->data->death_occured)
+        return ;
     t_philo *philo;
 
     philo = (t_philo *)philosopher;
     philo->state = 0;
     print_status(philo, 0); //thiking
+    if (philo->data->death_occured)
+        return ;
     pthread_mutex_lock(philo->left_fork);
     print_status(philo, 1);
-
+    if (philo->data->death_occured)
+        return ;
     pthread_mutex_lock(&philo->right_fork);
     print_status(philo, 1); //forks taken
-
+    if (philo->data->death_occured)
+        return ;
     philo->state = 1;
     philo->last_meal_time = get_time() - philo->data->init_time;
     philo->n_meals++;
     print_status(philo, 0);
     my_sleep(philo->data->time_to_eat);
-
+    if (philo->data->death_occured)
+        return ;
     pthread_mutex_unlock(&philo->right_fork);
     pthread_mutex_unlock(philo->left_fork);
 }
@@ -81,17 +93,15 @@ void    *monitor_routine(void *philosopher)
         //printf("philo %d time dincr last meal %lld\n", philo->index + 1 , get_time() - philo->data->init_time - philo->last_meal_time);
         if (philo->state  != 1 &&  get_time() - philo->data->init_time - philo->last_meal_time >= philo->data->time_to_die)
         {
-         
             philo->is_dead = 1;
             print_status(philo, 0);
             philo->data->death_occured = 1;
             return (NULL);
         }
-        if (everyone_ate(philo->data)) {
-            philo->data->death_occured = 1;
-            break ;
+        if ( everyone_ate(philo->data)) {
+            return (0);
         }
-        // usleep(100);
+        //usleep(20);
     }
     return (0);
 }
@@ -116,6 +126,7 @@ void    *start_routine(void *philosopher)
 void init_philos(t_data *data)
 {
     int i; 
+
 
     data->philosophers = (t_philo **)malloc(sizeof(t_philo *) * data->number_of_philosophers);
     i = 0;
@@ -146,6 +157,8 @@ void init_threads(t_data *data)
     int i;
 
     i = 0;
+    data->death_occured = 0;
+
     while (i < data->number_of_philosophers)
     {   
         if(!i)
